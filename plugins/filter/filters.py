ecb96b778b9aa790a409f1889489033ddcf392f0
Lucas Arbiza <lucas.arbiza@gmail.com>
2023-05-01 07:26:05 +0200

    Added a filter module, ended the stop/reload part

---
+++ plugins/filter/filters.py
@@ -0,0 +1,13 @@
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'extra_chains': self.return_user_defined_chains,
+        }
+
+
+
+    def return_user_defined_chains(self, output: list) -> list:
+        std = ['INPUT', 'OUTPUT', 'FORWARD']        
+        return [l.split(' ')[1] for l in output if l.split(' ')[1] not in std]
